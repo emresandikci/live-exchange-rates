b1463fe51b13a94d402c145fec744c4a17a584a8
emresandikci <me@emresandikci.com>
2019-08-20 00:55:56 +0300

    pages added

---
+++ src/pages/Settings/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<h1>Settings</h1>)
+  }
+}
+
+export default Settings;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export {default as Currencies} from './Currencies';
+export {default as Coins} from './Coins';
+export {default as Settings} from './Settings';
+export {default as Contact} from './Contact';
