657827133b62e702141b9ddb57ce59d93b9419ad
emresandikci <me@emresandikci.com>
2019-08-20 00:54:42 +0300

    updated

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Header } from './header';
 export { default as Table } from './table';
 export { default as AppRouter } from './appRouter';
-export { default as Menu } from './menu';
--- src/helper/index.js
+++ src/helper/index.js
@@ -2,3 +2,4 @@ export { default as colors } from './colors';
 export { default as ResetCss } from './reset-css';
 export { default as api } from './api';
 export { default as currencyStatus } from './currencyStatus';
+export { default as routes } from './routes';
