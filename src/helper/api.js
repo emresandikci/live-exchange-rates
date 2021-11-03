b55eee1121e5122eb00e56f84fb2b8efab1cc8ea
emresandikci <me@emresandikci.com>
2021-11-03 23:07:47 +0300

    fix: rate urls updated

--- src/helper/api.js
+++ src/helper/api.js
@@ -1,5 +1,5 @@
  export default {
-  url: "https://www.investing.com/currencies/streaming-forex-rates-majors",
+  url: "https://www.investing.com/currencies/streaming-forex-rates-majors?__cf_chl_jschl_tk__=Gyb5_DBh6rSD_sFNCZfjZGUK2Qy0zcP0aoTnCLvJRA4-1635969794-0-gaNycGzNBf0",
   urlCrypto: "https://www.investing.com/crypto/currencies",
   proxy: "https://no-cors-es.herokuapp.com/",//"https://cors-anywhere.herokuapp.com/",
   options: {
